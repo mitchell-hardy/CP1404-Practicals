4b46aafb5283620f5ce8f969bb4aaf781ab3e972
Mitchell hardy <mitchell.hardy@my.jcu.edu.au>
2017-03-18 06:00:22 +1000

    Included comments and minor code edits.

--- Week 4/fiveNumbers.py
+++ Week 4/fiveNumbers.py
@@ -1,25 +1,25 @@
 __author__ = 'Mitch Hardy'
 
 def main():
-
+#Take 5 user inputs into a list and print formatting baes information.
     user_numbers = get_numbers()
     print_numbers(user_numbers)
 
 
 def get_numbers():
+    #Get number inputs from user
     numbers = []
     for number in range(0,5):
-        number_input = float(input("{}---Please enter number: ".format(number+1)))
-        numbers.append(number_input)
-
+        input_number = int(input("Number: "))
+        numbers.append(input_number)
     return numbers
 
 def print_numbers(user_numbers):
+    #Print required values to user
     print("The first number is {}.".format(user_numbers[0]))
     print("The last number is {}.".format(user_numbers[-1]))
     print("The smallest number is {}.".format(min(user_numbers)))
     print("The largest number is {}.".format(max(user_numbers)))
-    print(sum(user_numbers))
     print("The average of the numbers is {}.".format(float(sum(user_numbers))/(len(user_numbers))))
 
 
