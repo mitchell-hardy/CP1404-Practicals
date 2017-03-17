04b4bd2c3504ad9beffae191d5ec41c076dc1575
Mitchell hardy <mitchell.hardy@my.jcu.edu.au>
2017-03-17 20:38:48 +1000

    String formatted income input. Refactored variables, refactored report printing into function.

--- Week 4/totalIncome.py
+++ Week 4/totalIncome.py
@@ -9,15 +9,19 @@ Starter code for cumulative total income program
 
 def main():
     incomes = []
-    months = int(input("How many months? "))
+    quantity_months = int(input("How many months? "))
 
-    for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
+    for month in range(1, quantity_months + 1):
+        income = float(input("Enter income for month {}:".format(month)))
         incomes.append(income)
 
+    print_report(incomes, quantity_months)
+
+
+def print_report(incomes, quantity_months):
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, months + 1):
+    for month in range(1, quantity_months + 1):
         income = incomes[month - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
