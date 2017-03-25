09e3f98f5e1653a55da1069d886889e9e6f44b88
Mitchell hardy <mitchell.hardy@my.jcu.edu.au>
2017-03-25 12:59:43 +1000

    Final file following Pylint checks and edits.

--- Week 5/state_names.py
+++ Week 5/state_names.py
@@ -1,24 +1,30 @@
 """
 CP1404/CP5632 Practical
 State names in a dictionary
-File needs reformatting
 """
 __author__ = 'Mitch Hardy'
 
 
-STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-               "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+def main():
+    """
+    State Names Dictionary Exercise.
+    """
 
-# Print(STATE_NAMES).
-for key, value in STATE_NAMES.items():
-    print("{} is {}".format(key, value))
+    STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory",
+                   "WA": "Western Australia", "ACT": "Australian Capital Territory",
+                   "VIC": "Victoria", "TAS": "Tasmania"}
 
-# Get sate appreviation from user and display full name.
-state = input("Enter short state: ").upper()
-while state != "":
-    if state in STATE_NAMES:
-        print(state, "is", STATE_NAMES[state])
-    else:
-        print("Invalid short state")
+    # Print(STATE_NAMES).
+    for key, value in STATE_NAMES.items():
+        print("{} is {}".format(key, value))
+
+    # Get sate abbreviation from user and display full name.
     state = input("Enter short state: ").upper()
+    while state != "":
+        if state in STATE_NAMES:
+            print(state, "is", STATE_NAMES[state])
+        else:
+            print("Invalid short state")
+        state = input("Enter short state: ").upper()
 
+main()
