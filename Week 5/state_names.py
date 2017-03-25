49b127a9ef87bdb90e6ff4897063eaea819ed116
Mitchell hardy <mitchell.hardy@my.jcu.edu.au>
2017-03-25 12:53:03 +1000

    Completed exercise including dictionary print and .upper addition to user input field.

--- Week 5/state_names.py
+++ Week 5/state_names.py
@@ -6,16 +6,19 @@ File needs reformatting
 __author__ = 'Mitch Hardy'
 
 
+STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+               "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
-STATE_NAMES = {"QLD":"Queensland", "NSW": "New South Wales", "NT" :"Northern Territory", "WA" : "Western Australia", "ACT":"Australian Capital Territory", "VIC":"Victoria", "TAS":"Tasmania"}
-# print(STATE_NAMES)
+# Print(STATE_NAMES).
+for key, value in STATE_NAMES.items():
+    print("{} is {}".format(key, value))
 
-state = input("Enter short state: ")
+# Get sate appreviation from user and display full name.
+state = input("Enter short state: ").upper()
 while state != "":
     if state in STATE_NAMES:
         print(state, "is", STATE_NAMES[state])
     else:
         print("Invalid short state")
-    state = input("Enter short state: ")
+    state = input("Enter short state: ").upper()
 
