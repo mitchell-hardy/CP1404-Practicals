6128b341d265da51e0e91d5218c2d9a8c733f5c4
Mitchell hardy <mitchell.hardy@my.jcu.edu.au>
2017-03-26 17:55:37 +1000

    Code operational, final formatting and checks needed.

--- Week 5/lists_dictionary.py
+++ Week 5/lists_dictionary.py
@@ -8,11 +8,8 @@ def lists_to_dictionary():
     dobs = []
     import _datetime
 
-
-
-
     for i in range(0, 1):
-        name = input("Please enter the name for person {}:".format(i+1).lower())
+        name = input("Please enter the name for person {}:".format(i+1)).lower()
         names.append(name)
         dob = ""
         correct_dob = False
@@ -25,30 +22,25 @@ def lists_to_dictionary():
                 print("Incorrect DOB format, please enter as DD/MM/YYYY!")
         dobs.append(dob)
 
+    names_dobs_dict = dict(zip(names, dobs))
+
+    age_query = input("Please enter a name to see their age: ").lower()
+    while age_query not in names_dobs_dict:
+        print("We have no data for that person, please choose from these people: ")
+        for key in names_dobs_dict:
+            print(key, end="\n")
+        age_query = input("Please enter a name to see their age: ").lower()
 
 
-        #         correct_dob = _datetime.date.strftime(check_dob, "%d/%m/%Y")
-        #     except ValueError:
-        #         p
-        # dobs.append(dob)
 
+    if age_query in names_dobs_dict:
+        for key, value in names_dobs_dict.items():
+            todays_date = _datetime.date.today()
+            current_year = int(todays_date.strftime("%Y"))
+            person_born = int(value[6:])
+            persons_age = current_year - person_born
+            print("{} is {} years old.".format(key, persons_age).capitalize())
 
-
-    #
-    # print(names)
-    # print(dobs)
-
-    # names_dobs_dict = dict(zip(names, dobs))
-    #
-    # age_query = input("Please enter a name to see their age: ").lower
-    # while age_query not in names_dobs_dict:
-    #     print("We have no data for that person, please choose from these persons: ")
-    #     for key in names_dobs_dict:
-    #         print(key, end="\n")
-    #     age_query = input("Please enter a name to see their age: ").lower
-    #
-    # if age_query in names_dobs_dict:
-    #     for key, value in names_dobs_dict.items():
-    #         print("{} was born on {}".format(key, value))
+            # print("{} was born on {}".format(persons_age))
 
 lists_to_dictionary()
